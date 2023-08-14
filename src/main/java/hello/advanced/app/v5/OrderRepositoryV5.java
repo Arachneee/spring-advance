cdfd1d396aa557ad698fd41994d2b832cf114b16
Arachne <jhg2819@naver.com>
2023-08-14 15:30:59 +0900

    templatecallback

---
+++ src/main/java/hello/advanced/app/v5/OrderRepositoryV5.java
@@ -0,0 +1,36 @@
+package hello.advanced.app.v5;
+
+
+import org.springframework.stereotype.Repository;
+
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+public class OrderRepositoryV5 {
+
+    private final TraceTemplate template;
+
+    public OrderRepositoryV5(LogTrace logTrace) {
+        this.template = new TraceTemplate(logTrace);
+    }
+
+    public void save(String itemId) {
+        template.execute("OrderRepository.save()", () -> {
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+            return "ok";
+        });
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
